25603215b36cfc99c18ea680bd9484e81fc3f53c
Michael Park <mparkcode@gmail.com>
2018-05-10 13:39:51 +0000

    Rearanged code & added single player click count game

--- chicken_7.js
+++ chicken_7.js
@@ -80,7 +80,8 @@ for (var i = 0; i < 24; i++) {
 var hiddenPics = document.querySelectorAll('.hiddenCards');
 var displayCards = document.querySelectorAll(".displayCards");
 
-//Player object constructor and creating players
+//-------------------------------------------Player object constructor and creating players
+
 function Player(active, lives, position, target){
   this.active = active;
   this.lives = lives;
@@ -94,28 +95,35 @@ function Player(active, lives, position, target){
   };
 }
 
-var Player1 = new Player(true, 1, 0, 1);
+var Player1 = new Player(false, 1, 0, 1);
 var Player2 = new Player(false, 1, 12, 13);
 var Player3 = new Player(false, 1, 15, 16);
 var Player4 = new Player(false, 1, 17, 18);
 
 
-//Asks for how many players and creates starting positions and array of non current players
+
+
+//------------------------Asks for how many players and creates starting positions and array of non current players
+
+
 var playerNumPrompt = prompt("Please enter number of players");
 var numPlayers = parseInt(playerNumPrompt);
 var nonCurrentPlayers = [Player1];
 
 
 if(numPlayers === 2){
+  Player1.active = true;
   Player2.active = true;
   nonCurrentPlayers.push(Player2);
 } else if(numPlayers === 3){
+  Player1.active = true;
   Player2.active = true;
   Player3.active = true;
   Player2.position = 7;
   Player2.target = 8;
   nonCurrentPlayers.push(Player2, Player3);
 } else if(numPlayers === 4){
+  Player1.active = true;
   Player2.active = true;
   Player3.active = true;
   Player4.active = true;
@@ -126,13 +134,8 @@ if(numPlayers === 2){
   nonCurrentPlayers.push(Player2, Player3, Player4);
 }
 
-var currentPlayer = nonCurrentPlayers.shift();
 
-function addActiveClasses(num){
-  currentPlayer.currentPosition().classList.remove('player' + num);
-  currentPlayer.currentPosition().classList.add('player'+num+'-active');
-  currentPlayer.targetPosition().classList.add('player'+num+'-target');
-}
+//------------------------Sets player details for active players
 
 function setPlayerDetails(player, num){
   if(player.active){
@@ -153,13 +156,22 @@ setPlayerDetails(Player1,1);
 setPlayerDetails(Player2,2);
 setPlayerDetails(Player3,3);
 setPlayerDetails(Player4,4);
-currentPlayer.currentPosition().classList.add('current-player');
-addActiveClasses(1);
 
-for(var i in nonCurrentPlayers){
-  nonCurrentPlayers[i].currentPosition().classList.add("non-current-player");
+//-------------------------------------------------Functions for use in the game
+
+//Adds active classes to a player if they become currentPlayer
+function addActiveClasses(num){
+  currentPlayer.currentPosition().classList.remove('player' + num);
+  currentPlayer.currentPosition().classList.add('player'+num+'-active');
+  currentPlayer.targetPosition().classList.add('player'+num+'-target');
 }
 
+//Removes the player color & current-player class on current game piece
+        function removeClasses(num){
+          currentPlayer.currentPosition().classList.remove('player'+num+'-active');
+          currentPlayer.currentPosition().classList.remove('current-player');
+          currentPlayer.targetPosition().classList.remove('player'+num+'-target');
+        }
 
 // changes the target position
         function checkPosition(num){
@@ -192,38 +204,7 @@ for(var i in nonCurrentPlayers){
           }
         }
 
-
-for (var i = 0; i < hiddenPics.length; i++) {
-  if(hiddenPics[i].classList.contains('cover')){
-    hiddenPics[i].addEventListener('click', function(){
-      let piece = this;
-      this.classList.remove('cover');
-      this.firstChild.classList.remove('hidden');
-      setTimeout(function(){
-        piece.classList.add('cover');
-        piece.firstChild.classList.add('hidden');
-      },1000);
-      
-      if(this.firstChild.src === currentPlayer.targetPosition().firstChild.src){
-
-        //Removes the player color & current-player class on current game piece
-        function removeClasses(num){
-          currentPlayer.currentPosition().classList.remove('player'+num+'-active');
-          currentPlayer.currentPosition().classList.remove('current-player');
-          currentPlayer.targetPosition().classList.remove('player'+num+'-target');
-        }
-        
-        if(currentPlayer === Player1){
-          removeClasses(1);
-        } else if(currentPlayer === Player2){
-          removeClasses(2);
-        } else if(currentPlayer === Player3){
-          removeClasses(3);
-        } else if(currentPlayer === Player4){
-          removeClasses(4);
-        }
-
-        //take all  players lives if jumped over
+//take all  players lives if jumped over
         function livesToTake(){
           var a;
           var b;
@@ -243,126 +224,133 @@ for (var i = 0; i < hiddenPics.length; i++) {
             }
           }
         }
-          
-        livesToTake();
-        
-        document.getElementById("p1lives").textContent = Player1.lives;
-        document.getElementById("p2lives").textContent = Player2.lives;
-        document.getElementById("p3lives").textContent = Player3.lives;
-        document.getElementById("p4lives").textContent = Player4.lives;
 
-        //checks for winner
-        if(currentPlayer.lives == nonCurrentPlayers.length+1){
-          alert("Winner!");
-          nonCurrentPlayers.push(currentPlayer);
-          for(var i in nonCurrentPlayers){
-            nonCurrentPlayers[i].currentPosition().classList = "";
-            nonCurrentPlayers[i].targetPosition().classList = "";
+//checks for winner
+        function checkWinner(){
+          if(currentPlayer.lives == nonCurrentPlayers.length+1){
+            alert("Winner!");
+            nonCurrentPlayers.push(currentPlayer);
+            for(var i in nonCurrentPlayers){
+              nonCurrentPlayers[i].currentPosition().classList = "";
+              nonCurrentPlayers[i].targetPosition().classList = "";
+            }
+            currentPlayer = "";
+            nonCurrentPlayers = [];
+            document.getElementById("life-count").style.display = "none";
           }
-          currentPlayer = "";
-          nonCurrentPlayers = [];
-          document.getElementById("life-count").style.display = "none";
         }
 
-        //changes the current position for next move
-        currentPlayer.position = currentPlayer.target;
-        
-        //adds the player color & current-player class to the current position
+//adds the player color & current-player class to the current position
         function addActiveColorAndCurrentClass(player, num){
           if(currentPlayer === player){
             currentPlayer.currentPosition().classList.add('player'+num+'-active');
             currentPlayer.currentPosition().classList.add('current-player');
           }
         }
-        addActiveColorAndCurrentClass(Player1,1);
-        addActiveColorAndCurrentClass(Player2,2);
-        addActiveColorAndCurrentClass(Player3,3);
-        addActiveColorAndCurrentClass(Player4,4);
-        
-        getTarget();
 
-          //adds colors to the target
-          if(currentPlayer === Player1){
-            currentPlayer.targetPosition().classList.add('player1-target');
-          } else if(currentPlayer === Player2){
-            currentPlayer.targetPosition().classList.add('player2-target');
-          } else if(currentPlayer === Player3){
-            currentPlayer.targetPosition().classList.add('player3-target');
-          } else if(currentPlayer === Player4){
-            currentPlayer.targetPosition().classList.add('player4-target');
-          }
-
-
-       }else if (this.firstChild.src !== currentPlayer.targetPosition().firstChild.src){
-
-        //changes the current player in case of incorrect guess
-        
-        //Removes active colors and classes and adds non active colors and classes
-        currentPlayer.currentPosition().classList.remove("current-player");
-        currentPlayer.currentPosition().classList.add("non-current-player");
-        function removeActiveClasses(player,num){
+//Removes active colors and classes and adds non active colors and classes        
+function removeActiveClasses(player,num){
           if(currentPlayer == player){
             currentPlayer.currentPosition().classList.remove("player"+num+"-active");
             currentPlayer.targetPosition().classList.remove("player"+num+"-target");
             currentPlayer.currentPosition().classList.add("player"+num);
           }
         }
+
+//------------------------Sets current-player and non-current-player classes for game start
+
+var currentPlayer = nonCurrentPlayers.shift();
+currentPlayer.currentPosition().classList.add('current-player');
+addActiveClasses(1);
+
+for(var i in nonCurrentPlayers){
+  nonCurrentPlayers[i].currentPosition().classList.add("non-current-player");
+}
+
+
+
+//------------------------------------------------------Multi Player game
+
+if(numPlayers > 1){
+  document.getElementById("clicks").style.display = "none";
+  for (var i = 0; i < hiddenPics.length; i++) {
+  if(hiddenPics[i].classList.contains('cover')){
+    hiddenPics[i].addEventListener('click', function(){
+      let piece = this;
+      this.classList.remove('cover');
+      this.firstChild.classList.remove('hidden');
+      setTimeout(function(){
+        piece.classList.add('cover');
+        piece.firstChild.classList.add('hidden');
+      },1000);
+      
+      if(this.firstChild.src === currentPlayer.targetPosition().firstChild.src){
+        
+        //removes active classes from current players current position
+        if(currentPlayer === Player1){
+          removeClasses(1);
+        } else if(currentPlayer === Player2){
+          removeClasses(2);
+        } else if(currentPlayer === Player3){
+          removeClasses(3);
+        } else if(currentPlayer === Player4){
+          removeClasses(4);
+        }
+        
+        //takes lives if jumping over another player
+        livesToTake();
+        
+        //updates scores on screen
+        document.getElementById("p1lives").textContent = Player1.lives;
+        document.getElementById("p2lives").textContent = Player2.lives;
+        document.getElementById("p3lives").textContent = Player3.lives;
+        document.getElementById("p4lives").textContent = Player4.lives;
+        
+        //Checks for a winner
+        checkWinner();
+  
+        //changes the current position for next move
+        currentPlayer.position = currentPlayer.target;
+        
+        //Adds active colors to the current players new position  
+        addActiveColorAndCurrentClass(Player1,1);
+        addActiveColorAndCurrentClass(Player2,2);
+        addActiveColorAndCurrentClass(Player3,3);
+        addActiveColorAndCurrentClass(Player4,4);
+        
+        //Gets terget for the next move  
+        getTarget();
+
+        //adds colors to the target
+        if(currentPlayer === Player1){
+          currentPlayer.targetPosition().classList.add('player1-target');
+        } else if(currentPlayer === Player2){
+          currentPlayer.targetPosition().classList.add('player2-target');
+        } else if(currentPlayer === Player3){
+          currentPlayer.targetPosition().classList.add('player3-target');
+        } else if(currentPlayer === Player4){
+          currentPlayer.targetPosition().classList.add('player4-target');
+        }
+
+
+      } else if (this.firstChild.src !== currentPlayer.targetPosition().firstChild.src){
+
+        //changes the current player in case of incorrect guess
+        currentPlayer.currentPosition().classList.remove("current-player");
+        currentPlayer.currentPosition().classList.add("non-current-player");
+        
+        //Removes active classes from the current player  
         removeActiveClasses(Player1,1);
         removeActiveClasses(Player2,2);
         removeActiveClasses(Player3,3);
         removeActiveClasses(Player4,4);
-        // if(currentPlayer == Player1){
-        //   currentPlayer.currentPosition().classList.remove("player1-active");
-        //   currentPlayer.targetPosition().classList.remove("player1-target");
-        //   currentPlayer.currentPosition().classList.add("player1");
-        // } else if(currentPlayer == Player2){
-        //   currentPlayer.currentPosition().classList.remove("player2-active");
-        //   currentPlayer.targetPosition().classList.remove("player2-target");
-        //   currentPlayer.currentPosition().classList.add("player2");
-        // } else if(currentPlayer == Player3){
-        //   currentPlayer.currentPosition().classList.remove("player3-active");
-        //   currentPlayer.targetPosition().classList.remove("player3-target");
-        //   currentPlayer.currentPosition().classList.add("player3");
-        // } else if(currentPlayer == Player4){
-        //   currentPlayer.currentPosition().classList.remove("player4-active");
-        //   currentPlayer.targetPosition().classList.remove("player4-target");
-        //   currentPlayer.currentPosition().classList.add("player4");
-        // }
         
         //Changes the current player
         nonCurrentPlayers.push(currentPlayer);
         currentPlayer = nonCurrentPlayers.shift();
         
+        //Gets Target for new current player  
         getTarget();
-        // if(currentPlayer.position === 20 && document.getElementById("square-21").classList.contains("non-current-player") && document.getElementById("square-22").classList.contains("non-current-player") && document.getElementById("square-23").classList.contains("non-current-player")){
-        //   currentPlayer.target = 0;
-        // } else if(currentPlayer.position === 21 && document.getElementById("square-22").classList.contains("non-current-player") && document.getElementById("square-23").classList.contains("non-current-player") && document.getElementById("square-0").classList.contains("non-current-player")){
-        //   currentPlayer.target = 1;
-        // } else if(currentPlayer.position === 21 && document.getElementById("square-22").classList.contains("non-current-player") && document.getElementById("square-23").classList.contains("non-current-player")){
-        //   currentPlayer.target = 0;
-        // } else if(currentPlayer.position === 22 && document.getElementById("square-23").classList.contains("non-current-player") && document.getElementById("square-0").classList.contains("non-current-player") && document.getElementById("square-1").classList.contains("non-current-player")){
-        //   currentPlayer.target = 2;
-        // } else if(currentPlayer.position === 22 && document.getElementById("square-23").classList.contains("non-current-player") && document.getElementById("square-0").classList.contains("non-current-player")){
-        //   currentPlayer.target = 1;
-        // } else if(currentPlayer.position === 22 && document.getElementById("square-23").classList.contains("non-current-player")){
-        //   currentPlayer.target = 0;
-        // } else if(currentPlayer.position === 23 && document.getElementById("square-0").classList.contains("non-current-player") && document.getElementById("square-1").classList.contains("non-current-player") && document.getElementById("square-2").classList.contains("non-current-player")){
-        //   currentPlayer.target = 3;
-        // } else if(currentPlayer.position === 23 && document.getElementById("square-0").classList.contains("non-current-player") && document.getElementById("square-1").classList.contains("non-current-player")){
-        //   currentPlayer.target = 2;
-        // } else if(currentPlayer.position === 23 && document.getElementById("square-0").classList.contains("non-current-player")){
-        //   currentPlayer.target = 1;
-        // } else if(!(currentPlayer.position === 23) && document.getElementById("square-" + (currentPlayer.position + 1)).classList.contains("non-current-player") && document.getElementById("square-" + (currentPlayer.position + 2)).classList.contains("non-current-player") && document.getElementById("square-" + (currentPlayer.position + 3)).classList.contains("non-current-player")){
-        //   currentPlayer.target = currentPlayer.position + 4;
-        // } else if (!(currentPlayer.position === 23) && document.getElementById("square-" + (currentPlayer.position + 1)).classList.contains("non-current-player") && document.getElementById("square-" + (currentPlayer.position + 2)).classList.contains("non-current-player")){
-        //   currentPlayer.target = currentPlayer.position + 3;
-        // } else if (!(currentPlayer.position === 23) && document.getElementById("square-" + (currentPlayer.position + 1)).classList.contains("non-current-player")){
-        //   currentPlayer.target = currentPlayer.position + 2;
-        // } else if(currentPlayer.position === 23){
-        //   currentPlayer.target = 0;
-        // } else {
-        //   currentPlayer.target = currentPlayer.position + 1;
-        // }
         
         //Adds active colors and classes and removes non active colors and classes
         currentPlayer.currentPosition().classList.remove("non-current-player");
@@ -380,7 +368,59 @@ for (var i = 0; i < hiddenPics.length; i++) {
     });
   }
 }
+}
 
+//-------------------------------------------------Single Player game
+if(numPlayers === 1){
 
-
+  var difficulty = prompt("Enter difficulty");
+  var clicksLeft;
+  if(difficulty == "easy"){
+    clicksLeft = 120;
+  }
+  if(difficulty == "medium"){
+    clicksLeft = 80;
+  }
+  if(difficulty == "difficult"){
+    clicksLeft = 40;
+  }
+  
+  document.getElementById("click-counter").textContent = clicksLeft;
+  
+  for (var i = 0; i < hiddenPics.length; i++) {
+    if(hiddenPics[i].classList.contains('cover')){
+      hiddenPics[i].addEventListener('click', function(){
+        let piece = this;
+        this.classList.remove('cover');
+        this.firstChild.classList.remove('hidden');
+        setTimeout(function(){
+          piece.classList.add('cover');
+          piece.firstChild.classList.add('hidden');
+        },1000);
+        if(this.firstChild.src === currentPlayer.targetPosition().firstChild.src){
+          removeClasses(1);
+          currentPlayer.position = currentPlayer.target;
+          getTarget();
+          if(currentPlayer.position == 0 && currentPlayer.target == 1){
+            alert("winner");
+            removeActiveClasses(Player1,1);
+            currentPlayer.currentPosition().classList.remove("player1");
+            currentPlayer = "";
+          }
+          addActiveColorAndCurrentClass(Player1,1);
+          currentPlayer.targetPosition().classList.add('player1-target');
+        } else {
+          clicksLeft -= 1;
+          if(clicksLeft == 0){
+            alert("loser");
+            removeActiveClasses(Player1,1);
+            currentPlayer.currentPosition().classList.remove("player1");
+            currentPlayer = "";
+          }
+          document.getElementById("click-counter").textContent = clicksLeft;
+        }
+      });
+    }  
+  }
+}
 
